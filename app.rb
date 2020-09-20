64913ee7281e8034bbbe00bb5b4eaee8ec753799
Olivia Cubela <oliviacubela@gmail.com>
2020-09-20 18:07:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,23 +14,4 @@ class App < Sinatra::Base
     "My favorite song is Toxic"
   end
 end
-  # def call(env)
-  #   resp = Rack::Response.new
-  #   req = Rack::Request.new(env)
-  #
-  #   if req.path=="/name"
-  #     resp.write "My name is  #{name}"
-  #   end
 
-    # if req.path=="/name"
-    #   resp.write "My name is #{name}"
-
-    # elsif req.path=="/hometown"
-    #   resp.write "My hometown is #{hometown}"
-    # else req.path=="/favorite-song"
-    #   resp.write "My favorite song is #{favorite-song}"
-    # end
-
-#     resp.finish
-#   end
-# end
